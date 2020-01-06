6ce94976e5a0ab1e9dbfafdef0e2f6f5874a6a34
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-01-06 21:05:27 +0100

    Fixes the cron race condition.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to **OPcache Manager** are documented in this *changelog*.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and **OPcache Manager** adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased - will be 1.1.1]
+### Fixed
+- A race condition can lead to "holes" in daily graphs.
 
 ## [1.1.0] - 2020-01-03
 ### Added
--- includes/features/class-capture.php
+++ includes/features/class-capture.php
@@ -68,15 +68,15 @@ class Capture {
 			$cache_id = '/Data/LastCheck';
 			$old      = Cache::get_global( $cache_id );
 			if ( ! isset( $old ) ) {
-				Logger::debug( 'No OPcache transient.' );
+				Logger::emergency( 'No OPcache transient.' );
 			} elseif ( ! array_key_exists( 'timestamp', $old ) ) {
-				Logger::debug( 'No OPcache timestamp.' );
+				Logger::emergency( 'No OPcache timestamp.' );
 			} elseif ( 300 - self::$delta > $time - $old['timestamp'] ) {
-				Logger::debug( sprintf( 'Delta time too short: %d sec. Launching recycling process.', $time - $old['timestamp'] ) );
+				Logger::emergency( sprintf( 'Delta time too short: %d sec. Launching recycling process.', $time - $old['timestamp'] ) );
 			} elseif ( 300 + self::$delta < $time - $old['timestamp'] ) {
-				Logger::debug( sprintf( 'Delta time too long: %d sec. Launching recycling process.', $time - $old['timestamp'] ) );
+				Logger::emergency( sprintf( 'Delta time too long: %d sec. Launching recycling process.', $time - $old['timestamp'] ) );
 			}
-			if ( isset( $old ) && array_key_exists( 'timestamp', $old ) && ( 300 - self::$delta < $time - $old['timestamp'] ) && ( 300 + self::$delta - $old['timestamp'] ) ) {
+			if ( isset( $old ) && array_key_exists( 'timestamp', $old ) && ( 300 + self::$delta > $time - $old['timestamp'] ) ) {
 				try {
 					$restart            = false;
 					$value              = [];
@@ -175,6 +175,8 @@ class Capture {
 				} catch ( \Throwable $e ) {
 					Logger::error( sprintf( 'Unable to query OPcache status: %s.', $e->getMessage() ), $e->getCode() );
 				}
+			} elseif ( isset( $old ) && array_key_exists( 'timestamp', $old ) && ( 300 - self::$delta > $time - $old['timestamp'] ) ) {
+				// What to do when delta is less 59 sec ?
 			} else {
 				try {
 					$value              = [];
