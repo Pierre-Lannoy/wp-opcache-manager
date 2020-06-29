b27e6dc163bced0f433dc356683d7e0bf0493969
Pierre Lannoy <pierre@lannoy.fr>
2020-06-29 10:43:22 +0200

    Ready to release version 1.3.2 on wp.org.

--- opcache-manager.php
+++ opcache-manager.php
@@ -10,7 +10,7 @@
  * Plugin Name:       OPcache Manager
  * Plugin URI:        https://github.com/Pierre-Lannoy/wp-opcache-manager
  * Description:       OPcache statistics and management right in the WordPress admin dashboard.
- * Version:           1.3.1
+ * Version:           1.3.2
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Pierre Lannoy
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: analytics, cache, monitor, OPcache, Zend
 Requires at least: 5.2
 Requires PHP: 7.2
 Tested up to: 5.4
-Stable tag: 1.3.1
+Stable tag: 1.3.2
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
