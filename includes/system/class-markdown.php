b00c6a0b562a5b573a8d9d3047ad35a0b304c413
Pierre Lannoy <pierre@lannoy.fr>
2020-09-28 09:29:40 +0200

    Fixes wrong path.

--- includes/system/class-markdown.php
+++ includes/system/class-markdown.php
@@ -45,7 +45,7 @@ class Markdown {
 		$mode        = $_attributes['mode'];
 		$error       = esc_html__( 'Sorry, unable to find or read the specified file.', 'opcache-manager' );
 		$result      = esc_html( $error );
-		$changelog   = DECALOG_PLUGIN_DIR . $file;
+		$changelog   = OPCM_PLUGIN_DIR . $file;
 		if ( file_exists( $changelog ) ) {
 			try {
 				// phpcs:ignore
