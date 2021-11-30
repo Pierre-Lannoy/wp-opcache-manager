8467ae34af0b4d1d0fef93d97d3ee396fcea2732
Pierre Lannoy <pierre@lannoy.fr>
2021-11-30 19:52:11 +0100

    Adds button in settings to install recommended plugins.

--- admin/class-opcache-manager-admin.php
+++ admin/class-opcache-manager-admin.php
@@ -325,6 +325,16 @@ class Opcache_Manager_Admin {
 								}
 							}
 							break;
+						case 'install-decalog':
+							if ( class_exists( 'PerfOpsOne\Installer' ) ) {
+								$result = \PerfOpsOne\Installer::do( 'decalog', true );
+								if ( '' === $result ) {
+									add_settings_error( 'opcm_no_error', '', esc_html__( 'Plugin successfully installed and activated with default settings.', 'opcache-manager' ), 'info' );
+								} else {
+									add_settings_error( 'opcm_install_error', '', sprintf( esc_html__( 'Unable to install or activate the plugin. Error message: %s.', 'opcache-manager' ), $result ), 'error' );
+								}
+							}
+							break;
 					}
 					break;
 			}
@@ -404,6 +414,9 @@ class Opcache_Manager_Admin {
 		} else {
 			$help  = '<img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'alert-triangle', 'none', '#FF8C00' ) . '" />&nbsp;';
 			$help .= sprintf( esc_html__('Your site does not use any logging plugin. To log all events triggered in OPcache Manager, I recommend you to install the excellent (and free) %s. But it is not mandatory.', 'opcache-manager' ), '<a href="https://wordpress.org/plugins/decalog/">DecaLog</a>' );
+			if ( class_exists( 'PerfOpsOne\Installer' ) && ! Environment::is_wordpress_multisite() ) {
+				$help .= '<br/><a href="' . esc_url( admin_url( 'admin.php?page=opcm-settings&tab=misc&action=install-decalog' ) ) . '" class="poo-button-install"><img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'download-cloud', 'none', '#FFFFFF', 3 ) . '" />&nbsp;&nbsp;' . esc_html__('Install It Now', 'opcache-manager' ) . '</a>';
+			}
 		}
 		add_settings_field(
 			'opcm_plugin_options_logger',
