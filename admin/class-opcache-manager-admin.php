657dcf6ee482e75cc27e4645ac797c1999546325
Pierre Lannoy <pierre@lannoy.fr>
2023-03-02 17:02:59 +0100

    Fixes a CSRF vulnerability / [CVE-2023-27444](https://www.cve.org/CVERecord?id=CVE-2023-27444) discovered by [Mika](https://patchstack.com/database/researcher/5ade6efe-f495-4836-906d-3de30c24edad) from [Patchstack](https://patchstack.com)

--- admin/class-opcache-manager-admin.php
+++ admin/class-opcache-manager-admin.php
@@ -312,6 +312,7 @@ class Opcache_Manager_Admin {
 		if ( ! ( $action = filter_input( INPUT_GET, 'action' ) ) ) {
 			$action = filter_input( INPUT_POST, 'action' );
 		}
+		$nonce = filter_input( INPUT_GET, 'nonce' );
 		if ( $action && $tab ) {
 			switch ( $tab ) {
 				case 'misc':
@@ -326,7 +327,7 @@ class Opcache_Manager_Admin {
 							}
 							break;
 						case 'install-decalog':
-							if ( class_exists( 'PerfOpsOne\Installer' ) ) {
+							if ( class_exists( 'PerfOpsOne\Installer' ) && $nonce && wp_verify_nonce( $nonce, $action ) ) {
 								$result = \PerfOpsOne\Installer::do( 'decalog', true );
 								if ( '' === $result ) {
 									add_settings_error( 'opcm_no_error', '', esc_html__( 'Plugin successfully installed and activated with default settings.', 'opcache-manager' ), 'info' );
@@ -415,7 +416,7 @@ class Opcache_Manager_Admin {
 			$help  = '<img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'alert-triangle', 'none', '#FF8C00' ) . '" />&nbsp;';
 			$help .= sprintf( esc_html__('Your site does not use any logging plugin. To log all events triggered in OPcache Manager, I recommend you to install the excellent (and free) %s. But it is not mandatory.', 'opcache-manager' ), '<a href="https://wordpress.org/plugins/decalog/">DecaLog</a>' );
 			if ( class_exists( 'PerfOpsOne\Installer' ) && ! Environment::is_wordpress_multisite() ) {
-				$help .= '<br/><a href="' . esc_url( admin_url( 'admin.php?page=opcm-settings&tab=misc&action=install-decalog' ) ) . '" class="poo-button-install"><img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'download-cloud', 'none', '#FFFFFF', 3 ) . '" />&nbsp;&nbsp;' . esc_html__('Install It Now', 'opcache-manager' ) . '</a>';
+				$help .= '<br/><a href="' . wp_nonce_url( admin_url( 'admin.php?page=opcm-settings&tab=misc&action=install-decalog' ), 'install-decalog', 'nonce' ) . '" class="poo-button-install"><img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'download-cloud', 'none', '#FFFFFF', 3 ) . '" />&nbsp;&nbsp;' . esc_html__('Install It Now', 'opcache-manager' ) . '</a>';
 			}
 		}
 		add_settings_field(
